ff77890d6ad076f77dd034d03c605d983801a666
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-01-24 21:13:25 -0500

    first-push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import { Route } from 'react-router-dom';
+import CharacterCard from "./components/CharacterCard.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <Route exact path="/" component={CharacterList} />
+      <Route path="/character/:id" component={CharacterCard} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const id = props.match.params.id;
+
+      axios 
+        .get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(response => {
+          setCharacter(response.data.results);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+        
+  }, []);
+
+  if (!character) {
+    return <div>Loading character information...</div>;
+  }
+  
+  const { id, name, status, species } = character;
+  return (
+    <div>
+      <div key={id}>
+        <h2>{name}</h2>
+        <div>Status: {status}</div>
+        <div>Species: {species}</div>
+      </div>
+    </div>
+  );
 }
