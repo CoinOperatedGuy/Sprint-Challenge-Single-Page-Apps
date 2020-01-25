d416abdcefe065863465a6d62f2b179ee2943200
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-01-24 22:04:58 -0500

    fixed issue

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 
 export default function CharacterCard(props) {
   const [character, setCharacter] = useState();
-  console.log('props', props)
-  useEffect((props) => {
+  // console.log('props', props)
+  useEffect(() => {
+    console.log('props', props)
     const id = props.match.params.id;
 
       axios 
         .get(`https://rickandmortyapi.com/api/character/${id}`)
         .then(response => {
-          setCharacter(response.data.results);
+          setCharacter(response.data);
+          console.log('single data', response.data)
         })
         .catch(error => {
           console.error(error);
@@ -22,11 +24,13 @@ export default function CharacterCard(props) {
     return <div>Loading character information...</div>;
   }
   
-  const { id, name, status, species } = character;
+  const { id, name, image, origin, status, species } = character;
   return (
     <div>
       <div key={id}>
         <h2>{name}</h2>
+        <img src={image} />
+        <div>Origin: {origin}</div>
         <div>Status: {status}</div>
         <div>Species: {species}</div>
       </div>
